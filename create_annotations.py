c54c7d114fde3702fb52d1767c171503e7c906a6
David MacNaughton <davidmacnaugh@gmail.com>
2021-10-05 17:41:46 +1000

    Params main.py --box2seg --yolo_subdir --path <dir or file.txt>

--- create_annotations.py
+++ create_annotations.py
@@ -1,40 +1,40 @@
-def create_image_annotation(file_name, width, height, image_id):
-    file_name = file_name.split('/')[-1] # ~~~.jpg가 file_name이 되도록 문자열 split
-    images = {
-        'file_name': file_name,
-        'height': height,
-        'width': width,
-        'id': image_id
+from pathlib import Path
+
+
+def create_image_annotation(file_path: Path, width: int, height: int, image_id: int):
+    file_path = file_path.name
+    image_annotation = {
+        "file_name": file_path,
+        "height": height,
+        "width": width,
+        "id": image_id,
     }
-    return images
+    return image_annotation
 
-def create_annotation_yolo_format(min_x, min_y, width, height, image_id, category_id, annotation_id):
-    bbox = (min_x, min_y, width, height)
+
+def create_annotation_from_yolo_format(
+    min_x, min_y, width, height, image_id, category_id, annotation_id, segmentation=True
+):
+    bbox = (float(min_x), float(min_y), float(width), float(height))
     area = width * height
-
+    max_x = min_x + width
+    max_y = min_y + height
+    if segmentation:
+        seg = [[min_x, min_y, max_x, min_y, max_x, max_y, min_x, max_y]]
+    else:
+        seg = []
     annotation = {
-        'id': annotation_id,
-        'image_id': image_id,
-        'bbox': bbox,
-        'area': area,
-        'iscrowd': 0,
-        'category_id': category_id,
-        'segmentation': []
+        "id": annotation_id,
+        "image_id": image_id,
+        "bbox": bbox,
+        "area": area,
+        "iscrowd": 0,
+        "category_id": category_id,
+        "segmentation": seg,
     }
 
     return annotation
 
-# Create the annotations of the ECP dataset (Coco format)
-coco_format = {
-    "images": [
-        {
-        }
-    ],
-    "categories": [
 
-    ],
-    "annotations": [
-        {
-        }
-    ]
-}
+# Create the annotations of the ECP dataset (Coco format)
+coco_format = {"images": [{}], "categories": [], "annotations": [{}]}
